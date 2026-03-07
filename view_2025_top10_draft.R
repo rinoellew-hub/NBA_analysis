d34188462240c622bac3ba075a823852fcc54cfb
Rinoelle Wisco <rinoellew@gmail.com>
2026-03-07 00:58:06 -0800

    Changed to top ten draft picks in the 2025 draft, using their last 5 games at the time of this change. Also combined the excel sheets via Tablaeu

---
+++ view_2025_top10_draft.R
@@ -0,0 +1,8 @@
+#install necessary packages for data analysis
+library(readr, tidyverse)
+df_2025draft <- read_csv("2025draft_10_picks.csv")
+View(df_2025draft)
+
+
+
+
--- view_team_history.R
+++
@@ -1,5 +0,0 @@
-#install necessary packages for data analysis
-library(readr)
-houston_rockets_history <- read_csv("houston_rockets_history.csv")
-View(houston_rockets_history)
-
